4475267d42d53e27c15cb0729dd1ccee31416a91
Jean Giordani <jeangiordani290@gmail.com>
2024-01-08 17:01:31 -0300

    feat: started coding the cards on mock exams list

--- src/pages/MockExams/index.tsx
+++ src/pages/MockExams/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
-import { Container, Content } from "./styles";
+import { Container, ContentContainer, Content, Cards } from "./styles";
 import { Sidebar } from "../../components/Sidebar";
+
+import mockExamIcon from "../../assets/icons/file-done-svgrepo-com.svg";
 
 const MockExams = () => {
     return (
@@ -10,7 +12,41 @@ const MockExams = () => {
             <Header />
             <Container>
                 <Sidebar />
-                <Content>a</Content>
+                <ContentContainer>
+                    <Content>
+                        <h2 className="title">Simulados</h2>
+                        <Cards>
+                            <div className="card">
+                                <img
+                                    src={mockExamIcon}
+                                    alt=""
+                                    className="card-icon"
+                                />
+                                <div className="card-title">
+                                    Meu primeiro simulado
+                                </div>
+                            </div>
+                            <div className="card">
+                                <img src="" alt="" className="card-icon" />
+                                <div className="card-title">
+                                    Meu primeiro simulado
+                                </div>
+                            </div>
+                            <div className="card">
+                                <img src="" alt="" className="card-icon" />
+                                <div className="card-title">
+                                    Meu primeiro simulado
+                                </div>
+                            </div>
+                            <div className="card">
+                                <img src="" alt="" className="card-icon" />
+                                <div className="card-title">
+                                    Meu primeiro simulado
+                                </div>
+                            </div>
+                        </Cards>
+                    </Content>
+                </ContentContainer>
             </Container>
             <Footer />
         </>
--- src/pages/MockExams/styles.ts
+++ src/pages/MockExams/styles.ts
@@ -8,7 +8,60 @@ export const Container = styled.div`
     justify-content: center;
 `;
 
-export const Content = styled.main`
+export const ContentContainer = styled.main`
     flex-grow: 1;
-    margin: 0 1rem;
+    /* margin: 0 1.5rem; */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Content = styled.div`
+    width: 95%;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .title {
+        font-family: "Roboto-Bold";
+        font-size: 1.2rem;
+        color: var(--primary);
+        margin-bottom: 1rem;
+        width: 100%;
+        margin: 10px 0;
+        overflow: hidden;
+    }
+`;
+
+export const Cards = styled.div`
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 100%;
+
+    .card {
+        width: 200px;
+        height: 300px;
+        border-radius: 10px;
+        border: 1px solid black;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .card-icon {
+            margin: 0 auto;
+            width: 50px;
+            height: 50px;
+            filter: var(--primary-filter);
+        }
+
+        .card-title {
+            font-family: "Roboto-Bold";
+            font-size: 1.2rem;
+            color: var(--primary);
+            text-align: center;
+        }
+    }
 `;
